97825efb96780ba369800f85c7fa7b8a869430a7
Your0nlySon <eaduncan18@gmail.com>
2021-11-17 11:47:32 -0500

    fxml files

---
+++ application2/src/main/java/baseline/InventoryManagementApplication.java
@@ -0,0 +1,26 @@
+package baseline;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+
+public class InventoryManagementApplication extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("InventoryManagementApplication.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Todo List");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
